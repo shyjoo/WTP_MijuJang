da7263fd4e78bdaa533c680d7fce40acc927c978
shyjoo <shyjoo@live.com>
2017-08-19 23:45:20 -0400

    set project

---
+++ shopify_MijuJang/shopify_MijuJang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shopify_MijuJang
+//
+//  Created by Lily Jang on 2017-08-19.
+//  Copyright © 2017 Lily Jang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
